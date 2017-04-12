11f6571e308d83c472202f40173be1c0a13a2064
Paul M. Bendixen <paulbendixen@gmail.com>
2017-04-13 01:56:09 +0200

    Intial commit
    Everything seems to be working,
    except I get the wrong value at front when pushing beyond the container

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ring.h>
+#include <array>
+
+int main()
+{
+	std::array< int, 3 > a;
+
+	sg14::ring_span< int > r( a.begin(), a.end() );
+
+	r.push_back( 1 );
+	r.push_back( 2 );
+	r.push_back( 3 );
+	r.push_back( 5 );
+
+	assert( r.front() == 2 );
+
+}
